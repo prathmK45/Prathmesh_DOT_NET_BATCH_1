f64fab20d840fe9709b62d5bce1a3c845e2639ec
prathmK45 <prathmeshkhile9930@gmail.com>
2024-09-04 23:17:09 +0530

    Two Datatype Assignment

---
+++ Assignments/assignment1st/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using DataType;
+
+class Assignment1{
+    public static void Main(){
+            _INT a = new _INT(10);
+            _INT b = new _INT(20);
+            Console.WriteLine(a.division(a,b));
+            Console.WriteLine(a.addition(a,b));
+            Console.WriteLine(b.subtraction(a,b));
+
+            _FLOAT c = new _FLOAT(10.5);
+            _FLOAT d = new _FLOAT(20.6);
+            Console.WriteLine(c.division(a,b));
+            Console.WriteLine(c.addition(a,b));
+            Console.WriteLine(d.subtraction(a,b));
+        }
+}
